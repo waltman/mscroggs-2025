ce088814d6f83577f860111a7c2de3c70f384d68
Walt Mankowski <waltman@pobox.com>
2025-12-22 09:22:19 -0500

    added code for day 22

---
+++ day22.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+eq1 = lambda a, b, c: (a*b) + c == 11
+eq2 = lambda a, b, c: (a/b) / c == 1
+eq3 = lambda a, b, c: (a+b) - c == 1
+eq4 = lambda a, b, c: (a*b) - c == 1
+eq5 = lambda a, b, c: (a/b) - c == 0
+eq6 = lambda a, b, c: (a+b) / c == 1
+
+for arr in permutations(range(1,10)):
+    if eq1(arr[0], arr[1], arr[2]) and \
+       eq2(arr[3], arr[4], arr[5]) and \
+       eq3(arr[6], arr[7], arr[8]) and \
+       eq4(arr[0], arr[3], arr[6]) and \
+       eq5(arr[1], arr[4], arr[7]) and \
+       eq6(arr[2], arr[5], arr[8]):
+
+       print(arr)
+       print(arr[3] * arr[6] * arr[8])
